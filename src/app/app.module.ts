a4d51cf984a1f8e4dbb38cd9b9faac688bc32b99
Nelson Dias <nelson.dias@polarising.com>
2017-05-24 09:06:09 +0100

    Creating 2 routes - dashboard and list of heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 
 // import of all app components
 import { appComponent } from 'app/app.component';
+import { dashboardComponent } from 'app/dashboard/dashboard.component';
 import { heroesComponent } from 'app/heroes/heroes.component';
 import { heroDetailComponent } from './heroDetail/heroDetail.component';
 import { superHeroesService } from './services/superheroes.service';
@@ -14,6 +15,7 @@ import { superHeroesService } from './services/superheroes.service';
 @NgModule({
   declarations: [
     appComponent,
+    dashboardComponent,
     heroesComponent,
     heroDetailComponent
   ],
@@ -22,11 +24,22 @@ import { superHeroesService } from './services/superheroes.service';
     BrowserModule,
     FormsModule,
     HttpModule,
-    //array of route definitions
+    //array of routes configuration
     RouterModule.forRoot([
+      /* Make sure when the app starts, it should show the dashboard
+       and display a /dashboard URL in the browser address bar. */
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
       {
         path: 'heroes',
         component: heroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: dashboardComponent
       }
     ])
   ],
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,18 @@
+/**
+ * Created by nelson.dias on 24/05/2017.
+ */
+
+import { Component } from '@angular/core';
+
+
+//dashboard decorator
+@Component({
+
+  selector: 'my-dashboard',
+  templateUrl: 'dashboard.component.html'
+})
+
+export class dashboardComponent{
+
+
+}
